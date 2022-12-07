330bf4892e5d140b7ff6e0e96e1b4802be638606
Justin Randall <jusranda@cisco.com>
2022-12-07 00:01:18 -0500

    Added code documentation.

--- src/client/voice-trigger-workflows.js
+++ src/client/voice-trigger-workflows.js
@@ -1,12 +1,50 @@
+/**
+ * Copyright 2022 David Finnegan, Justin Randall, Cisco Systems Inc. All Rights Reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { LitElement, html } from "lit";
 import style from './style.scss';
 
-
+/**
+ * Implementation for WxCC Voice Trigger Workflow widget for the WxCC Agent Desktop
+ * 
+ * @author David Finnegan
+ */
 export class VoiceTriggerWorkflows extends LitElement {
+
+  /**
+   * Access handler for style assets.
+   * 
+   * @returns the style contents of style.scss.
+   */
   static get styles() {
     return [style]
   }
 
+  /**
+   * The widget properties.
+   * 
+   * @static
+   * @type {Object}
+   */
   static properties = {
     darkmode: {},
     configJSON: {},
@@ -17,8 +55,11 @@ export class VoiceTriggerWorkflows extends LitElement {
     _showBtn: {state: true},
     _config: {state: true},
     _selected: {sate: true}
-   }
+  }
 
+  /**
+   * Construct a new instance.
+   */
   constructor() {
     super();
     this.darkmode = null;
@@ -33,7 +74,9 @@ export class VoiceTriggerWorkflows extends LitElement {
     this._selected = null;
   }
 
-
+  /**
+   * Invoked when the component is added to the document's DOM.
+   */
   connectedCallback() {
     super.connectedCallback()
     this._theme = this.setTheme();
@@ -42,7 +85,21 @@ export class VoiceTriggerWorkflows extends LitElement {
     this._config = JSON.parse(JSON.stringify(this.configJSON));
   }
 
+  /**
+   * Invoked when the component is removed from the document's DOM.
+   */
+  disconnectedCallback() {
+    // TODO: What do I need to do there?
+  }
 
+  /**
+   * Updates the element. This method reflects property values to attributes
+   * and calls `render` to render DOM via lit-html. Setting properties inside
+   * this method will *not* trigger another update.
+   * 
+   * @param changedProperties Map of changed properties with old values
+   * @category updates
+   */
   updated(changedProperties) {
     if (changedProperties.has('darkmode')) { this._theme = this.setTheme() }
 
@@ -57,15 +114,18 @@ export class VoiceTriggerWorkflows extends LitElement {
     }
   }
 
-  disconnectedCallback() {
-
-  }
-
+  /**
+   * Sets the UI darkmode theme for the widget.
+   * 
+   * @returns "dark" if darkmode is enabled; otherwise, "light".
+   */
   setTheme() {
     return this.darkmode == 'true' ? "dark" : "light";
   }
 
-
+  /**
+   * Click handler for div id="btn"
+   */
   btnClicked() {
     if (this._mainIsOpen == false) {
       this._mainIsOpen = true;
@@ -79,14 +139,20 @@ export class VoiceTriggerWorkflows extends LitElement {
     }
    }
 
-
+  /**
+   * Click handler for div id="menu"
+   * 
+   * @param event The click event.
+   */
   menuClicked(event) {
     this._selected = event.target.getAttribute('data-id');
     this.shadowRoot.querySelector(`[data-modal-id="${this._selected}"]`).setAttribute("opened", "");
     this.shadowRoot.querySelector("#menu").removeAttribute("opened");
   }
 
-
+  /**
+   * Cancel widget click handler.
+   */
   cancelClicked() {
     const id = this._selected;
     this.shadowRoot.querySelector(`[data-modal-id="${id}"]`).removeAttribute("opened");
@@ -101,7 +167,19 @@ export class VoiceTriggerWorkflows extends LitElement {
     this._selected = null;
   }
 
-  triggerClicked(event){
+  /**
+   * Submit widget click handler.
+   * 
+   * @param event The click event.
+   */
+  submitClicked(event){
+    /**
+     * Invoke an HTTP fetch with a timeout.
+     * 
+     * @param {*} resource The URL.
+     * @param {*} options the HTTP operation control parameters.
+     * @returns the HTTP response.
+     */
     async function fetchWithTimeout(resource, options = {}) {
       const { timeout = 6000 } = options;  
       const controller = new AbortController();
@@ -111,7 +189,13 @@ export class VoiceTriggerWorkflows extends LitElement {
       return response;
     }
 
-    async function sendTrigger(url, body) {
+    /**
+     * Submit the form data to the HTTP handler.
+     * 
+     * @param {string} url  The HTTP URL.
+     * @param {Object} body The HTTP request body.
+     */
+    async function submitForm(url, body) {
       try {
           
         const response = await fetchWithTimeout(url, {
@@ -122,7 +206,9 @@ export class VoiceTriggerWorkflows extends LitElement {
         });
         const data = await response.json();
         console.log('voice-trigger-workflow Post Result', response.status, data);
-        
+
+        // TODO: Add error handling here.
+
       } catch (error) {
         console.log('voice-trigger-workflow Post Error', error);
       }
@@ -137,28 +223,31 @@ export class VoiceTriggerWorkflows extends LitElement {
         });
     }
 
- 
     let key = this.taskSelected.interactionId;
     let task = JSON.parse(JSON.stringify(this.taskMap.get(key)));
 
     body = {...body, ...task};
 
-    sendTrigger(this._config[id].url, body);
+    submitForm(this._config[id].url, body);
     this.cancelClicked();
-
   }
 
+  /**
+   * Dynamically set the input type based on the parameters.type value in the Agent Desktop layout.
+   * 
+   * @param {Object} action The action objcet containing the input parameters.
+   * @param {number} index  The action input parameter index.
+   * @returns the dyncamically typed HTML input markup.
+   */
   modalSectionTemplate(action, index){
     if (action.parameters) {
       return html`
         ${action.parameters.map(param => {
-
           if (param.type === 'input') {
             return html`
               <label>${param.label}</label>
               <input data-id-input="${index}" name="${param.name}" type="text">`
           }
-
           if (param.type === 'select') {
             return html`
               <label>${param.label}</label>
@@ -167,19 +256,25 @@ export class VoiceTriggerWorkflows extends LitElement {
               </select>
              `
           }
-
           if (param.type === 'datetime') {
             return html`
               <label>${param.label}</label>
               <input type="datetime-local" data-id-input="${index}" name="${param.name}">
             `
           }
-          
         })}
       `
     }
   }
 
+  /**
+   * Invoked on each update to perform rendering tasks. This method may return
+   * any value renderable by lit-html's `ChildPart` - typically a
+   * `TemplateResult`. Setting properties inside this method will *not* trigger
+   * the element to update.
+   * 
+   * @category rendering
+   */
   render() {
     return html`
       ${this._showBtn
@@ -206,7 +301,7 @@ export class VoiceTriggerWorkflows extends LitElement {
                     
                     <footer>
                       <button data-id="${index}" @click=${this.cancelClicked} class="cancel-btn">Cancel</button>
-                      <button data-id="${index}" @click=${this.triggerClicked} class="trigger-btn">Schedule</button>
+                      <button data-id="${index}" @click=${this.submitClicked} class="trigger-btn">Schedule</button>
                     </footer>
                   </div>
                 `)}
@@ -217,6 +312,5 @@ export class VoiceTriggerWorkflows extends LitElement {
     }`;
   }
 }
-
 
 customElements.define('voice-trigger-workflows', VoiceTriggerWorkflows);
